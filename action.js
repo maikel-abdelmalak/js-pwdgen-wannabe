c75bf2a5343b5e66788fc902ee1d639702627e92
maikel abdelmalak <maikel22@hotmail.it>
2020-04-17 15:45:34 +0200

    2

--- action.js
+++ action.js
@@ -1,7 +1,17 @@
+//creo una variabile in cui inserire il nome
 var nome = prompt ('come ti chiami?');
+
+//creo una variabile in cui inserire il cognome
 var cognome = prompt ('Quale è il tuo cognome?');
+
+//creo una variabile in cui inserire il colore
 var colore = prompt ('Quale è il tuo colore preferito?');
-var data = new Date();
+
+//creo una variabile in cui inserire la data
 var anno = '20';
+
+//creo una variabile in cui inserire concatenare le 4 stringhe
 var risultato = nome + cognome + colore + anno;
+
+//stampo la variabile risultato all'interno dello span password
 document.getElementById('password').innerHTML = risultato;
